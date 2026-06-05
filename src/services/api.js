af645e3f13eba70f6e4d3e943e4634b7f230254a
AqilUlhaq <aqilulhaq237@gmail.com>
2026-06-06 02:12:11 +0700

    Fix API URL and login endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: 'https://gdb-backend-production-4dd1.up.railway.app',
   headers: {
-    'Content-Type': 'application/json',
-  },
+    'Content-Type': 'application/json'
+  }
 });
 
 export default API;
